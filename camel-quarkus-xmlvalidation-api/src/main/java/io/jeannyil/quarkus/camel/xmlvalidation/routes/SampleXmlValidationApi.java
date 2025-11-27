8c758c70553280015a0bcde24d61df74f8ce818f
Jean Nyilimbibi <jean.nyilimbibi@gmail.com>
2025-11-27 12:19:00 +0100

    Renamed and cleaned up

--- camel-quarkus-xmlvalidation-api/src/main/java/io/jeannyil/quarkus/camel/xmlvalidation/routes/SampleXmlValidationApi.java
+++ camel-quarkus-xmlvalidation-api/src/main/java/io/jeannyil/quarkus/camel/xmlvalidation/routes/SampleXmlValidationApi.java
@@ -1,34 +1,29 @@
 package io.jeannyil.quarkus.camel.xmlvalidation.routes;
 
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
-import org.apache.camel.Exchange;
+import org.apache.camel.CamelContext;   
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.camel.model.rest.RestParamType;
 
-import io.jeannyil.quarkus.camel.xmlvalidation.models.ErrorResponse;
-
-/* Exposes the Sample XML Validation RESTful API
+/* Exposes the Sample XML Validation RESTful API with a Contract/API-First approach
 
 /!\ The @ApplicationScoped annotation is required for @Inject and @ConfigProperty to work in a RouteBuilder. 
 	Note that the @ApplicationScoped beans are managed by the CDI container and their life cycle is thus a bit 
 	more complex than the one of the plain RouteBuilder. 
 	In other words, using @ApplicationScoped in RouteBuilder comes with some boot time penalty and you should 
 	therefore only annotate your RouteBuilder with @ApplicationScoped when you really need it. */
-public class SampleXmlValidationApiRoute extends RouteBuilder {
+@ApplicationScoped
+public class SampleXmlValidationApi extends RouteBuilder {
+    private static String logName = SampleXmlValidationApi.class.getName();
 
-	private static String logName = SampleXmlValidationApiRoute.class.getName();
-
-	
-	@Override
-	public void configure() throws Exception {
-		
-		/**
-		 * Catch unexpected exceptions
-		 */
+    @Inject
+	CamelContext camelctx;
+    
+    @Override
+    public void configure() throws Exception {
+		// Catch unexpected exceptions
 		onException(Exception.class)
 			.handled(true)
 			.maximumRedeliveries(0)
